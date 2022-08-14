5d01aca25bf8131d05bb46689c3a17b8bc9807c5
Hassan <hassan.seedat07@gmail.com>
2022-08-14 23:20:28 +0100

    added in fetch for the weatherapi

--- weatherapi/src/components/Main/Main.jsx
+++ weatherapi/src/components/Main/Main.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 
 const Main = () => {
+
+    const [ searchTerm, setSearchTerm ] = useState("London");
+    const [ weatherObj, setWeatherObj ] = useState(false);
+
+    const getWeather = () => {
+        fetch(`http://api.weatherapi.com/v1/current.json?key=3f60da4d844e4b66ab2115419222403&q=${searchTerm}`
+        ).then(json => {
+            return json.json();
+        }).then(data => {
+            setWeatherObj(data);
+        })
+    }
+
   return (
-    <div>Main</div>
+    
   )
 }
 
