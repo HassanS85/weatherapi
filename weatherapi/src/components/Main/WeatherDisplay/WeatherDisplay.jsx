5b3bc817057472867aa41e57050cf321b48bfefd
Hassan <hassan.seedat07@gmail.com>
2022-08-15 15:30:23 +0100

    added fetch method to weatherdisplay

--- weatherapi/src/components/Main/WeatherDisplay/WeatherDisplay.jsx
+++ weatherapi/src/components/Main/WeatherDisplay/WeatherDisplay.jsx
@@ -3,9 +3,28 @@ import './WeatherDisplay.css';
 
 
 const WeatherDisplay = (props) => {
+    const weatherObj = props.weatherObj;
 
+    const postWeather = () => {
+      fetch("", {
+        method: "POST",
+        headers: {
+          'Accept': "application/JSON",
+          'Content-Type': "application/JSON"
+        },
+        body: JSON.stringify( {
+          location: weatherObj.location.name,
+          temp: weatherObj.current.temp_c,
+          imgUrl: weatherObj.current.condition.icon,
+          description: weatherObj.current.condition.text
+        })
+      })
+    }
 
-    
+    useEffect( () => {
+      postWeather()
+    },[weatherObj])
+
   return (
     <div>WeatherDisplay</div>
   )
